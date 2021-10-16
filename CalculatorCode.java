8367f437f1c2ecd52edff3d08efa49b5757373c3
Anas Khan <89466320+justlikebing@users.noreply.github.com>
2021-10-16 20:20:57 +0530

    Add files via upload
    Calculator code of doing operations on two number

---
+++ CalculatorCode.java
@@ -0,0 +1,41 @@
+package javaforplace;
+
+import java.util.Scanner;
+
+public class CalculatorCode {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the First number:");
+		int a = sc.nextInt();
+		System.out.println("Enter the second value:");
+		int b = sc.nextInt();
+		System.out.println("Enter the operand!");
+		sc.nextLine();
+		char op = sc.nextLine().charAt(0);
+
+		int result = 0;
+		switch (op) {
+		case '+':
+			result = a + b;
+			break;
+		case '-':
+			result = a - b;
+			break;
+		case '*':
+			result = a * b;
+			break;
+		case '/':
+			result = a / b;
+			break;
+		case '%':
+			result = a % b;
+			break;
+		default:
+			System.out.println("INVALID!!!!!!!");
+		}
+		System.out.println(result);
+
+	}
+
+}
