a0e3ff4e93caadff0f7e65e92955cedbdae07864
Anas Khan <89466320+justlikebing@users.noreply.github.com>
2024-06-03 16:50:21 +0530

    Update FactorialOfAnyNumber.java

--- FactorialOfAnyNumber.java
+++ FactorialOfAnyNumber.java
@@ -1,7 +1,6 @@
 package javaforplace;
 import java.util.Scanner;
 public class FactorialOfAnyNumber {
-
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("enter the value:");
